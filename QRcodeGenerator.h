dbc3742ca746bbd81bdd0bef104b240b263875e6
rasikaiceman <rasikalakshantm@gmail.com>
2024-08-03 15:14:03 +0530

    Updated Documentation

--- QRcodeGenerator.h
+++ QRcodeGenerator.h
@@ -15,8 +15,20 @@ namespace QRcodeGenerator {
     using namespace System::Drawing;
     using namespace System::Drawing::Drawing2D;
 
+    /// <summary>
+    /// A class to generate QR codes.
+    /// </summary>
+
     public ref class QRCodeGenerator {
     public:
+
+        /// <summary>
+    /// Generates a QR code from the provided text.
+    /// </summary>
+    /// <param name="text">The text to encode in the QR code.</param>
+    /// <param name="imgsize">The size of each QR code module.</param>
+    /// <returns>A bitmap image of the generated QR code.</returns>
+
         static System::Drawing::Bitmap^ generateQRCode(System::String^ text, int imgsize);
     };
 }
